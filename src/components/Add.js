63396f052a63072e497f27e0af6e658f5f039020
Deniz Saganak <saganak.deniz@gmail.com>
2023-03-10 11:01:08 +0100

    scaffold the app

---
+++ src/components/Add.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Add = () => {
+  return (
+    <div className='add-page'>
+      <div className='container'>
+        <div className='add-content'>
+          <div className='input-wrapper'>
+            <input type="text" placeholder='Search for a Movie' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Add
---
+++ src/components/Watchlist.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Watchlist = () => {
+  return (
+    <div>Watchlist Page</div>
+  )
+}
+
+export default Watchlist
