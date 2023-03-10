d064cd40ec54e79c69f5fb80d499b0bc12d6c9c6
Deniz Saganak <saganak.deniz@gmail.com>
2023-03-10 11:37:35 +0100

    start Add component

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,12 +1,28 @@
-import React from 'react'
+import { useState } from 'react';
 
 const Add = () => {
+  const [query, setQuery] = useState("");
+
+  const handleChange = e => {
+    e.preventDefault();
+    setQuery(e.target.value);
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+    })
+  }; 
   return (
     <div className='add-page'>
       <div className='container'>
         <div className='add-content'>
           <div className='input-wrapper'>
-            <input type="text" placeholder='Search for a Movie' />
+            <input 
+                value={query}
+                onChange={handleChange} 
+                type="text" 
+                placeholder='Search for a Movie' />
           </div>
         </div>
       </div>
