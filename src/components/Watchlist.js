4ca4eb13980027141ddb314757151c2bcb597dc1
Deniz Saganak <saganak.deniz@gmail.com>
2023-03-12 12:26:30 +0100

    use localStorage to map data

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+import MovieCard from './MovieCard';
 
 const Watchlist = () => {
+  const { watchlist } = useContext(GlobalContext);
+  console.log(watchlist)
+
   return (
-    <div>Watchlist Page</div>
+    <div className='movie-page'>
+     <div className='container'>
+      <div className='header'>
+        <h1 className='heading'>My Watchlist</h1>
+      </div>
+
+      <div className='movie-grid'>
+        {watchlist.map(movie => (
+          <MovieCard movie={movie} type="watchlist"/>
+        ))}
+      </div>
+     </div> 
+    </div>
   )
 }
 
