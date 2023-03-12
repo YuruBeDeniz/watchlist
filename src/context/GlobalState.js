a34b1fbe481a3ced35b3966be853d84f37263796
Deniz Saganak <saganak.deniz@gmail.com>
2023-03-12 11:45:55 +0100

    set up localStorage

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,9 +2,17 @@ import { createContext, useReducer, useEffect } from "react";
 import AppReducer from "./AppReducer";
 
 //to create context, we need an initial value which will be our store
+//to check if there is anything stored in our localStorage:
+//localStorage.getItem("watchlist") ? JSON.parse(localStorage.getItem("watchlist") 
+//above line of code checks if there is anything stored as "watchlist"; if there is,
+//we'll parse it so that it turns it back from a string to an array
 const initialState= {
-    watchlist: localStorage.getItem("watchlist") ? JSON.parse(localStorage.getItem("watchlist")) : [] ,
-    watched: [],
+    watchlist: localStorage.getItem("watchlist") 
+      ? JSON.parse(localStorage.getItem("watchlist")) 
+      : [] ,
+      watched: localStorage.getItem("watched") 
+      ? JSON.parse(localStorage.getItem("watched")) 
+      : [] ,
 };
 
 //to be able to provide GlobalContext to the other components, we need to make a
@@ -16,7 +24,8 @@ export const GlobalProvider = props => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
     useEffect(() => {
-      localStorage.setItem("watchlist", JSON.stringify(state.watchlist))
+      localStorage.setItem("watchlist", JSON.stringify(state.watchlist));
+      localStorage.setItem("watched", JSON.stringify(state.watched))
     }, [state])
 
     //actions
