28e1387447e1dfc83a9702d7d36eb910a80d920e
Deniz Saganak <saganak.deniz@gmail.com>
2023-03-16 10:01:52 +0100

    add env key

--- src/components/Add.js
+++ src/components/Add.js
@@ -9,7 +9,7 @@ const Add = () => {
     e.preventDefault();
     setQuery(e.target.value);
 
-    fetch(`https://api.themoviedb.org/3/search/movie?api_key=f8d7ef85079c340b3bc698c803d1d102&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1&include_adult=false&query=${e.target.value}`)
     .then(res => res.json())
     .then(data => {
       console.log(data)
